342125d4d8c1f8a0df900ff9add82ba417858a85
Angela Masajo <aabmasajo@gmail.com>
2020-12-30 20:55:15 -0600

    MainForm.js updated

--- src/App.js
+++ src/App.js
@@ -46,39 +46,6 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
-    
-    
     return (
       <div className="App">
         <header>
@@ -86,7 +53,11 @@ class App extends Component {
         </header>
         <main>
           <MainForm 
-            features={features}/>
+            features={this.props.features}
+            selected={this.state.selected} 
+            updateFeature={this.updateFeature}
+            USCurrencyFormat={USCurrencyFormat}
+            />
           <Cart 
             USCurrencyFormat={USCurrencyFormat}
             selectedOption={this.state.selected}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+//move to MainForm.js
 const FEATURES = {
   Processor: [
     {
